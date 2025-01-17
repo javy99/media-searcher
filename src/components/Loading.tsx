38feba454c83a5f980239ded38dcff83a227451c
jkosimov <javlonbek.kosimov@nokia.com>
2025-01-17 13:52:41 +0100

    create Loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Loading: React.FC = () => {
   return (
-    <div>Loading</div>
+    <div className='flex items-center justify-center py-4'>Loading...</div>
   )
 }
 
-export default Loading
+export default Loading;
