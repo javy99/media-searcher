b5c56dfa50bb33495df1cd7c277db51c30532fa6
jkosimov <javlonbek.kosimov@nokia.com>
2025-01-17 14:16:48 +0100

    creates SearchBar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
-const SearchBar: React.FC = () => {
+interface SearchBarProps {
+  searchItem: string;
+  setSearchItem: (searchItem: string) => void;
+  onSearch: () => void;
+  loading: boolean;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ 
+  searchItem,
+  setSearchItem,
+  onSearch,
+  loading
+}) => {
   return (
-    <div>SearchBar</div>
+    <div className="flex items-center gap-2">
+      <Label htmlFor='search'>Search</Label>
+      <Input
+        id='search'
+        type='search'
+        placeholder='Search here...'
+        value={searchItem}
+        onChange={(e) => setSearchItem(e.target.value)}
+      />
+      <button
+        onClick={onSearch}
+        disabled={loading}
+        className='bg-blue-500 text-white font-bold py-2 px-4 rounded disabled:bg-blue-300'
+      >
+        Search
+      </button>
+    </div>
   )
 }
 
