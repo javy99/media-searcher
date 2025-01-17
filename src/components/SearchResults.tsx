70ae6104ab085d4587bb27307748f629a3f71570
jkosimov <javlonbek.kosimov@nokia.com>
2025-01-17 16:07:08 +0100

    creates SearchResults component

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,7 +1,35 @@
+import { SearchResult } from '@/types';
 import React from 'react'
 
-const SearchResults: React.FC = () => {
-  return <div>SearchResults</div>;
+interface SearchResultsProps {
+  results: SearchResult[];
+}
+
+const SearchResults: React.FC<SearchResultsProps> = ({ results }) => {
+  if (!results || results.length === 0) {
+    return <p>No results found...</p>
+  }
+
+  return (
+    <div className="mt-4">
+      {results.map((result) => (
+        <div key={`${result.trackId}-${result.collectionName}`} className="border p-2 mb-2 rounded-md">
+          <div className="flex gap-2 items-center">
+            <img
+              src={result.artworkUrl100}
+              alt={result.trackName}
+              className="w-16 h-16 rounded"
+            />
+            <div>
+              <p className="font-bold">{result.trackName}</p>
+              <p className="text-gray-600">{result.artistName}</p>
+              <p className="text-gray-600">{result.primaryGenreName}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default SearchResults
