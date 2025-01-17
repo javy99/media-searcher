a0db87e285668d4b0143a384bc80e8670dc2bc35
jkosimov <javlonbek.kosimov@nokia.com>
2025-01-17 17:45:23 +0100

    updates fetchSearchResults to handle more params

--- src/services/iTunesApi.ts
+++ src/services/iTunesApi.ts
@@ -2,10 +2,40 @@ import { ITunesApiResponse, SearchResult } from "@/types";
 
 const BASE_URL = 'https://itunes.apple.com/search';
 
-export async function fetchSearchResults(term: string): Promise<SearchResult[] | null> {
-  try {
-    const response = await fetch(`${BASE_URL}?term=${term}&entity=song`);
+interface SearchOptions {
+  term: string;
+  country?: string;
+  media?: string;
+  entity?: string;
+  attribute?: string;
+  limit?: number;
+  lang?: string;
+  version?: number;
+  explicit: "Yes" | "No";
+}
 
+const buildQueryString = (options: SearchOptions): string => {
+  const params = new URLSearchParams();
+  console.log({options});
+  console.log({params});
+  for (const key in options) {
+    const optionKey = key as keyof SearchOptions;
+    if (options[optionKey] !== undefined) {
+      params.append(key, String(options[optionKey]));
+    }
+  }
+  console.log({ params });
+  console.log('params' + params.toString());
+
+  return params.toString();
+}
+
+export async function fetchSearchResults(options: SearchOptions): Promise<SearchResult[] | null> {
+  try {
+    const queryString = buildQueryString(options);
+    console.log({queryString});
+    const response = await fetch(`${BASE_URL}?${queryString}`);
+    console.log({response});
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
