17218c78d6ccfb4253e687b395eb40c94cdaa12c
jkosimov <javlonbek.kosimov@nokia.com>
2025-01-17 12:45:37 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
+const App = () => {
+  return (
+    <div className="p-4">
+      <div className="flex gap-2 items-center">
+        <Label htmlFor="search">Search</Label>
+        <Input id="search" type="search" placeholder="Search here..." />
+      </div>
+      <Button className="mt-4">Search</Button>
+    </div>
+  );
+}
+
+export default App;
