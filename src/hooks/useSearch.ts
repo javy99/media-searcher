5681eae16db98bfd8c43d34d99b475792f8f69d7
jkosimov <javlonbek.kosimov@nokia.com>
2025-01-17 17:57:58 +0100

    updates useSearch hook to pass options

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -2,17 +2,29 @@ import { fetchSearchResults } from "@/services/iTunesApi";
 import { SearchResult } from "@/types";
 import { useState } from "react"
 
+interface SearchOptions {
+  term: string;
+  country?: string;
+  media?: string;
+  entity?: string;
+  attribute?: string;
+  limit?: number;
+  lang?: string;
+  version?: number;
+  explicit?: "Yes" | "No";
+}
+
 const useSearch = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [results, setResults] = useState<SearchResult[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-  const search = async () => {
+  const search = async (options?:Omit<SearchOptions, "term">) => {
     setLoading(true);
     setError(null);
 
-    const searchResults = await fetchSearchResults(searchTerm);
+    const searchResults = await fetchSearchResults({term: searchTerm, ...options, explicit: options?.explicit ?? "No"});
 
     if (searchResults) {
       setResults(searchResults);
